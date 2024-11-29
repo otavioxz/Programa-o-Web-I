8610ecc48715bcb17a3b8c602c9c4398a2e114a8
otavio.rodrigues <contato.otaviorodrigues@gmail.com>
2024-11-29 20:25:00 -0300

    Delete Hospital directory

--- Hospital/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configurações do banco de dados
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'hospital_avaliacao');
-define('DB_USER', 'postgres');
-define('DB_PASS', 'postgres');
-define('DB_PORT' , '5433');
-?>
